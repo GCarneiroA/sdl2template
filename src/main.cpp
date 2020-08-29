5b1ecfabce102a29be3edad71494b4fd7d70fb34
Gustavo Carneiro <gcarneiroa@hotmail.com>
2020-08-29 05:30:44 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+
+
+#include <SDL2/SDL.h>
+
+
+
+int main(int argc, char **argv)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2template",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    while (true) {
+        // main application loop
+
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
